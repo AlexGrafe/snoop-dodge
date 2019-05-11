98c5be5fb38e5413a115b1942319a92ddfa99bb2
Alex Grafe <alex.grafe@xanderia.one>
2019-05-11 13:02:44 +0200

    Added to list

--- snoopdodge.sh
+++ snoopdodge.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-echo "Hello World"
+
+echo "Snoop Dodge"
+
+# KillAllList() {
+# area=$(($1 * $2))
+# echo "Area is : $area"
+# }
+
+applications=("edpa" "SymDaemon" "SymSharedSettingsd" "SymUIAgent" "TaniumClient" "TaniumRecorder" "TaniumEndpointIndex" "jamf" "jamfAgent" "JamfDaemon" "CrashPlanService" "Citrix Service Record Application" "Citrix Workspace Authentication")
+applicationsRegExp=""
+
+for a in ${applications[@]}; do
+  applicationsRegExp+="$a|"
+done
+
+# Remove last "|"
+applicationsRegExp=${applicationsRegExp%?}
+
+echo "ALL: $applicationsRegExp"
+
+sudo pkill -9 -f "/${applicationsRegExp}/"
+
+# KillAllList 10 20
+
+# sleep 1
